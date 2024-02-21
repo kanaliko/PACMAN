5892fff30ab970299d8cd60928a0b83d8704e356
kanaliko <160720820+kanaliko@users.noreply.github.com>
2024-02-21 11:49:31 +0100

    Create Pacman1.cpp
    Pacman 1

---
+++ Pacman1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Vitame ta v hre PACMAN!" << endl;
+    cout << "Verime, ze sa Ti hra bude pacit" << endl;
+    return 0;
+}
